d58381ab7af3e2c4561630fbf218d68c4c974fe9
juan c <juanchozass@gmail.com>
2022-11-11 22:49:18 +0100

    Button

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed } from "vue";
+import { COLORS } from "../commons";
+const { onClick, classes } = defineProps(["onClick", "classes"]);
+const classList = computed(() =>
+  classes && classes.length ? classes.join(" ") : " "
+);
+</script>
+
+<template>
+  <span :class="`round-button ${classList}`" @click="onClick">
+    <slot></slot>
+  </span>
+</template>
+
+<style scoped>
+.round-button {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+  padding: 5px;
+  height: 20px;
+  width: 20px;
+  border-radius: 20px;
+  border: 1px v-bind(COLORS.dark) solid;
+}
+</style>
