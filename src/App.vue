71271d7618786961d3a43c21168635297386497a
juan chozas <chozassumbera@gmail.com>
2022-11-13 10:34:28 +0100

    Fixes and tweaks

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,45 @@
 import { COLORS } from "./commons";
 import Paginator from "./components/Paginator.vue";
 import { ref, computed } from "vue";
-import movies from "./assets/movies.json";
+import MOVIES from "./assets/movies.json";
 import MovieCard from "./components/MovieCard.vue";
 import FilterPaginatorCard from "./components/PaginationCard.vue";
 import FilterCard from "./components/FilterCard.vue";
+import PrevNextButton from "./components/PrevNextButton.vue";
 
+const movies = MOVIES.map((m) =>
+  Object.assign({}, m, { score: Number(m.score) })
+);
+
+// Reactive vars
 const pageSize = ref(5);
 const currentPage = ref(0);
 const titleSearchQuery = ref("");
 const lastPage = computed(() =>
   Math.ceil(filteredMovies.value.length / pageSize.value)
 );
-const filteredMovies = computed(() =>
-  movies.filter((m) =>
-    titleSearchQuery !== ""
-      ? m.title
-          .toLowerCase()
-          .includes(titleSearchQuery.value.trim().toLowerCase())
-      : true
+const filteredMovies = computed(() => movies.filter((m) => filterByTitle(m)));
+const slicedMovies = computed(() =>
+  filteredMovies.value.slice(
+    currentPage.value * pageSize.value,
+    (currentPage.value + 1) * pageSize.value
   )
 );
+
+// Filters
+const filterByTitle = (movie) => {
+  if (titleSearchQuery.value.length < 2) return true;
+  const lowerCaseTitle = titleSearchQuery.value.toLowerCase();
+  return movie.title.toLowerCase().includes(lowerCaseTitle);
+};
+
+// Handlers
 const handleSearchTextChange = (newSearchQuery) => {
   titleSearchQuery.value = newSearchQuery;
   if (currentPage.value + 1 > lastPage.value) {
     currentPage.value = Math.max(lastPage.value - 1, 0);
   }
 };
-
 const handlePageSizeChange = (newPageSize) => {
   pageSize.value = newPageSize;
   if (currentPage.value + 1 > lastPage.value) {
@@ -45,32 +57,42 @@ const handlePageChange = (newPage) => {
   <FilterPaginatorCard
     :pageSize="pageSize"
     :numMovies="filteredMovies.length"
-    @onPageSizeChanged="handlePageSizeChange"
+    @pageSizeChanged="handlePageSizeChange"
   />
   <FilterCard
     :searchQuery="titleSearchQuery"
     @searchQueryChanged="handleSearchTextChange"
   />
-  <ul v-if="filteredMovies.length" class="movies-container">
-    <li
-      class="movie-card-container"
-      v-for="movie in filteredMovies.slice(
-        currentPage * pageSize,
-        currentPage * pageSize + pageSize
-      )"
-    >
-      <MovieCard :movie="movie" />
-    </li>
-  </ul>
-  <div class="zero-movies" v-else>
-    <h3>zero movies found =[</h3>
-    <p>you can try changing your search and/or filter though =]</p>
+  <div class="movies-container">
+    <PrevNextButton
+      :isPrev="true"
+      :onClick="() => (currentPage = currentPage - 1)"
+      :isOn="currentPage > 0"
+    />
+    <ul v-if="filteredMovies.length" class="movie-cards">
+      <li
+        class="movie-card-container"
+        v-for="movie of slicedMovies"
+        :key="movie.id"
+      >
+        <MovieCard :movie="movie" />
+      </li>
+    </ul>
+    <div class="zero-movies" v-else>
+      <h3>zero movies found =[</h3>
+      <p>you can try changing your search and/or filter though =]</p>
+    </div>
+    <PrevNextButton
+      :isPrev="false"
+      :onClick="() => (currentPage = currentPage + 1)"
+      :isOn="currentPage + 1 < lastPage"
+    />
   </div>
   <Paginator
     :currentPage="currentPage"
     :lastPage="lastPage"
     :nrPrevNextPages="2"
-    @onPageSelected="handlePageChange"
+    @pageSelected="handlePageChange"
   />
 </template>
 
@@ -93,6 +115,11 @@ const handlePageChange = (newPage) => {
 }
 
 .movies-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.movie-cards {
   width: 90vw;
   display: flex;
   flex-wrap: wrap;
